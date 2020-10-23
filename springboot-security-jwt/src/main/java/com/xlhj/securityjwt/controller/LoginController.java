718c6aaff21a4e4464e4f3afac98a766a5fda546
liucaijing <liucaijing@hanhua.com>
2020-10-23 21:27:52 +0800

    springboot整合springsecurity和jwt

---
+++ springboot-security-jwt/src/main/java/com/xlhj/securityjwt/controller/LoginController.java
@@ -0,0 +1,14 @@
+package com.xlhj.securityjwt.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: lcj
+ * @Date: 2020/10/23 17:42
+ * @Description: TODO
+ * @Version: 0.0.1
+ */
+@RestController
+public class LoginController {
+
+}
